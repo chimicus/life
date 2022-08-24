7733951d9a1069e9f1e966ab98365bbb5d75fd34
Emiliano Testa <etesta@undo.io>
2022-08-24 17:21:11 +0100

    Rewrote most of it, added two more unit tests

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@
 extern crate clap;
 use clap::{Arg, App, ArgMatches};
 use std::process;
+use std::ops::Shr;
+use std::ops::Sub;
 use std::fs::File;
 use std::io::{BufReader, BufRead, Error};
 use spin_sleep;
@@ -19,45 +21,145 @@ struct Point {
 	col: usize,
 }
 
-fn center_matrix(matrix: &mut Vec<Vec<bool>>, start: Point, end: Point) -> Result<(), Error> {
-	let half_length = Point {
-		row: (end.row - start.row) >> 1,
-		col: (end.col - start.col) >> 1,
-	};
-	let matrix_len = matrix.capacity();
-	let new_start = Point {
-		row: matrix_len - half_length.row,
-		col: matrix_len - half_length.col,
-	};
-	for line in &mut matrix.into_iter() {
-		line.rotate_right(new_start.col);
-	}
-	matrix.rotate_right(new_start.row);
-	Ok(())
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, p: Point) -> Point {
+        let r = Point {
+            row:  (self.row as i64 - p.row as i64).abs() as usize,
+            col:  (self.col as i64 - p.col as i64).abs() as usize
+        };
+        return r
+    }
+}
+impl Sub<usize> for Point {
+    type Output = Self;
+
+    fn sub(self, s: usize) -> Point {
+        let r = Point {
+            row:  (self.row as i64 - s as i64).abs() as usize,
+            col:  (self.col as i64 - s as i64).abs() as usize
+        };
+        return r
+    }
 }
 
-fn parse_file(input_file: &str, matrix: &mut Vec<Vec<bool>>) -> Result<(), Error> {
+impl Shr<usize> for Point {
+    type Output = Self;
+
+    fn shr(self, shr: usize) -> Self {
+        Self {row: self.row >> shr, col: self.col >> shr}
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Matrix {
+    m: Vec<Vec<bool>>,
+    size: usize
+}
+
+impl Matrix {
+
+    pub fn new(size: usize) -> Self {
+        Self { m: Vec::with_capacity(size), size: size }
+    }
+
+    fn capacity(&self) -> usize {
+        self.size
+    }
+
+    fn init(&mut self) {
+        for ii in 0..self.size {
+            self.m.push(Vec::with_capacity(self.size));
+            for _jj in 0..self.size {
+                self.m[ii].push(false)
+            }
+        }
+    }
+    fn update_matrix(&self) -> Matrix {
+        /* Rules for the update:
+         * - Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+         * - Any live cell with two or three live neighbours lives on to the next generation.
+         * - Any live cell with more than three live neighbours dies, as if by
+         * overpopulation.
+         * - Any dead cell with exactly three live neighbours becomes a live cell, as if
+         * by reproduction.
+         *
+         * Each cell has 8 neighbours and the plane has no boundaries (IE: wrap around)
+         *
+    	 */
+        let mut new_m = self.clone();
+        let m = self.m.clone();
+    	for (row_idx, line) in m.iter().enumerate() {
+    		for (col_idx, cell) in line.iter().enumerate() {
+    			let num_alive_neighbours = self.count_alive_neighbours(row_idx, col_idx);
+    			new_m.m[row_idx][col_idx] = *cell;
+    			if *cell {
+    				if num_alive_neighbours < 2 || num_alive_neighbours > 3 {
+    					new_m.m[row_idx][col_idx] = false;
+    				}
+    			} else {
+    				if num_alive_neighbours == 3 {
+    					new_m.m[row_idx][col_idx] = true;
+    				}
+    			}
+    		}
+    	}
+        return new_m;
+    }
+    fn count_alive_neighbours(&self, row_idx: usize, col_idx: usize) -> u32 {
+    	let start_row = if row_idx > 0 {row_idx - 1} else {(row_idx as isize - 1 + self.m.len() as isize) as usize};
+    	let start_col = if col_idx > 0 {col_idx - 1} else {(col_idx as isize - 1 + self.m[row_idx].len() as isize) as usize};
+    	let end_row = (row_idx + 1) % self.m.len();
+    	let end_col = (col_idx + 1) % self.m[row_idx].len();
+
+    	self.m[start_row][start_col] as u32 +
+        self.m[start_row][col_idx] as u32 +
+        self.m[start_row][end_col] as u32 +
+
+    	self.m[row_idx][start_col] as u32 +
+        self.m[row_idx][end_col] as u32 +
+
+    	self.m[end_row][start_col] as u32 +
+        self.m[end_row][col_idx] as u32 +
+        self.m[end_row][end_col] as u32
+    }
+}
+
+fn center(matrix: &mut Matrix , start: Point, end: Point) {
+    let half_length = (end - start) >> 1;
+    let new_start = half_length - matrix.capacity();
+    let m = &mut matrix.m;
+	for line in &mut m.into_iter() {
+		line.rotate_right(new_start.col);
+	}
+	matrix.m.rotate_right(new_start.row);
+}
+
+fn parse_file(input_file: &str, arena_size: usize) -> Result<Matrix, Error> {
+    let mut matrix = Matrix::new(arena_size);
+    matrix.init();
     let input = File::open(input_file)?;
     let buffered = BufReader::new(input);
     let mut col;
     let mut row = 0;
 	let mut start_point = Point {
-		row: matrix.capacity(),
-		col: matrix.capacity(),
+		row: arena_size,
+		col: arena_size,
 	};
 	let mut end_point = Point {
 		row: 0,
 		col: 0,
 	};
-    for line in buffered.lines() {//.expect("Cannot read file") {
+    for line in buffered.lines() {
         match line {
             Ok(l) => {
                 col = 0;
                 for c in l.chars() {
                     if row < matrix.capacity() &&
-                       col < matrix[row].capacity() {
+                       col < matrix.m[row].capacity() {
                         if c == '1' {
-                            matrix[row][col] = true;
+                            matrix.m[row][col] = true;
 							if row < start_point.row {
 								start_point.row = row;
 							}
@@ -72,7 +174,7 @@ fn parse_file(input_file: &str, matrix: &mut Vec<Vec<bool>>) -> Result<(), Error
 							}
                         }
                         else {
-                            matrix[row][col] = false;
+                            matrix.m[row][col] = false;
                         }
                     }
                     else {
@@ -87,10 +189,8 @@ fn parse_file(input_file: &str, matrix: &mut Vec<Vec<bool>>) -> Result<(), Error
         }
         row = row + 1;
     }
-    if let Err(_e) = center_matrix(matrix, start_point, end_point) {
-		println!("Problem centering the matrix");
-	}
-    Ok(())
+    center(&mut matrix, start_point, end_point);
+    Ok(matrix)
 }
 
 fn update_screen(matrix: &Vec<Vec<bool>>, iter: u32) {
@@ -105,67 +205,7 @@ fn update_screen(matrix: &Vec<Vec<bool>>, iter: u32) {
     }
 }
 
-fn count_alive_neighbours(matrix: &Vec<Vec<bool>>, row_idx: usize, col_idx: usize) -> u32 {
-	let start_row = if row_idx > 0 {row_idx - 1} else {(row_idx as isize - 1 + matrix.len() as isize) as usize};
-	let start_col = if col_idx > 0 {col_idx - 1} else {(col_idx as isize - 1 + matrix[row_idx].len() as isize) as usize};
-	let end_row = (row_idx + 1) % matrix.len();
-	let end_col = (col_idx + 1) % matrix[row_idx].len();
-
-	matrix[start_row][start_col] as u32 +
-    matrix[start_row][col_idx] as u32 +
-    matrix[start_row][end_col] as u32 +
-
-	matrix[row_idx][start_col] as u32 +
-    matrix[row_idx][end_col] as u32 +
-
-	matrix[end_row][start_col] as u32 +
-    matrix[end_row][col_idx] as u32 +
-    matrix[end_row][end_col] as u32
-}
-
-fn update_matrix(matrix: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    /* Rules for the update:
-     * - Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-     * - Any live cell with two or three live neighbours lives on to the next generation.
-     * - Any live cell with more than three live neighbours dies, as if by
-     * overpopulation.
-     * - Any dead cell with exactly three live neighbours becomes a live cell, as if
-     * by reproduction.
-     *
-     * Each cell has 8 neighbours and the plane has no boundaries (IE: wrap around)
-     *
-	 */
-    let mut new_matrix = init_arena(matrix.len());
-	for (row_idx, line) in matrix.iter().enumerate() {
-		for (col_idx, cell) in line.iter().enumerate() {
-			let num_alive_neighbours = count_alive_neighbours(&matrix, row_idx, col_idx);
-			new_matrix[row_idx][col_idx] = *cell;
-			if *cell {
-				if num_alive_neighbours < 2 || num_alive_neighbours > 3 {
-					new_matrix[row_idx][col_idx] = false;
-				}
-			} else {
-				if num_alive_neighbours == 3 {
-					new_matrix[row_idx][col_idx] = true;
-				}
-			}
-		}
-	}
-	new_matrix
-}
-
-fn init_arena(asize: usize) -> Vec<Vec<bool>> {
-    let mut matrix: Vec<Vec<bool>> = Vec::with_capacity(asize);
-    for ii in 0..asize {
-        matrix.push(Vec::with_capacity(asize));
-        for _jj in 0..asize {
-            matrix[ii].push(false)
-        }
-    }
-	matrix
-}
-
-fn run(matches: ArgMatches) -> Result<(), String> {
+fn run(matches: ArgMatches) -> Result<(), Error> {
 
     let input_file = matches.value_of("input file").unwrap();
     let ticks_str = matches.value_of("ticks");
@@ -200,16 +240,14 @@ fn run(matches: ArgMatches) -> Result<(), String> {
             }
         }
     }
-    let mut matrix = init_arena(arena_size);
 
-    if let Err(e) = parse_file(input_file, &mut matrix) {
-        println!("Couldn't parse file, error {}", e);
-    }
+    let mut matrix = parse_file(input_file, arena_size)?;
 	let spin_sleeper = spin_sleep::SpinSleeper::new(100_000);
     for ii in 0..num_ticks {
-        let new_matrix = update_matrix(&matrix);
-    	update_screen(&matrix, ii);
-		matrix = new_matrix;
+        let new_m = matrix.update_matrix();
+        matrix = new_m;
+    	update_screen(&matrix.m, ii);
+		//matrix = new_matrix;
 		spin_sleeper.sleep_ns(500_000_000);
     }
     Ok(())
@@ -252,7 +290,61 @@ mod tests {
     #[test]
     fn check_init() {
         let arena_size = 5;
-        let matrix =  init_arena(arena_size);
+        let matrix = Matrix::new(arena_size);
         assert_eq!(matrix.capacity(), 5);
+    }
+
+    #[test]
+    fn check_parse_file() {
+       let input_file = "./data/toad.txt";
+       let matrix = parse_file(input_file, 10).unwrap();
+       let data_correct = vec![
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, true,  true,  true,  false, false, false, false, false, false],
+           [true,  true,  true,  false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false],
+           [false, false, false, false, false, false, false, false, false, false]
+       ];
+       assert_eq!(data_correct.len(), matrix.m.len());
+       for lines in data_correct.iter().zip(matrix.m.iter())
+       {
+           let (correct_line, ut_line) = lines;
+           for cols in correct_line.iter().zip(ut_line.iter())
+           {
+               let (correct_col, ut_col) = cols;
+               assert_eq!(correct_col, ut_col);
+           }
+       }
+    }
+
+    #[test]
+    fn check_update_matrix() {
+       let input_file = "./data/toad.txt";
+       let matrix = parse_file(input_file, 5).unwrap();
+       let new_m = matrix.update_matrix();
+       let data_correct = vec![
+           [false, false, true,  false, false],
+           [true,  false, false, true,  false],
+           [true,  false, false, true,  false],
+           [false, true,  false, false, false],
+           [false, false, false, false, false],
+       ];
+       assert_eq!(data_correct.len(), new_m.m.len());
+       for lines in data_correct.iter().zip(new_m.m.iter())
+       {
+           println!("New line");
+           let (correct_line, ut_line) = lines;
+           for cols in correct_line.iter().zip(ut_line.iter())
+           {
+               let (correct_col, ut_col) = cols;
+               println!("correct: {}, ut: {}", correct_col, ut_col);
+               assert_eq!(correct_col, ut_col);
+           }
+       }
     }
 }
